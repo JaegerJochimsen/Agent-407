ffe06387b28d061f58f9a5c40762f677d5d0f24f
JaegerJochimsen <75139372+JaegerJochimsen@users.noreply.github.com>
2021-02-08 21:03:22 -0800

    Updated in order to include ammo tracking and reload

--- gunScript.cs
+++ gunScript.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 public class gunScript : MonoBehaviour
 {
@@ -6,6 +6,9 @@ public class gunScript : MonoBehaviour
     public float range = 100f;
     public float fireRate = 15f;
     public float impactForce = 30f;
+    static public int bullets = 100;
+    public int bulletsInMag = 10;
+    public float reloadDelay = -1f;
     
     public Camera fpsCam;
     public ParticleSystem muzzleFlash;
@@ -19,7 +22,44 @@ public class gunScript : MonoBehaviour
         if (Input.GetMouseButton(0) && Time.time >= nextTimeToFire) 
         {
             nextTimeToFire = Time.time + 1f / fireRate;
-            Shoot();
+            if (bulletsInMag > 0 && reloadDelay <= 0f)
+            // && reloadDelay <= 0f
+            {
+                Shoot();
+                Debug.Log("bullets: " + bullets);
+            }
+            else
+            {
+                Reload();
+                if(reloadDelay <= 0)
+                    reloadDelay = 1.5f;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.R) && bullets > 0)
+        {
+            Reload();
+            if (reloadDelay <= 0)
+                reloadDelay = 1.5f;
+        }
+
+            reloadDelay -= Time.deltaTime;
+    }
+    void Reload()
+    {
+        if (reloadDelay < 0)
+        {
+            int diff = 10 - bulletsInMag;
+            if (bullets > diff)
+            {
+                bullets -= diff;
+                bulletsInMag += diff;
+            }
+            else
+            {
+                bulletsInMag += bullets;
+                bullets = 0;
+            }
         }
     }
 
@@ -46,5 +86,6 @@ public class gunScript : MonoBehaviour
             GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
             Destroy(impactGO, 0.5f);
         }
+        bulletsInMag -= 1;
     }
 }
